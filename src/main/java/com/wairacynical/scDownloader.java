cb47a2bc483bdd72b55d3fc730aa48b5fee6b841
wairacynical <lieteeng@gmail.com>
2020-03-02 14:36:47 +0200

    new link algo, no more junk files

--- src/main/java/com/wairacynical/scDownloader.java
+++ src/main/java/com/wairacynical/scDownloader.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -15,9 +16,10 @@ import java.nio.file.StandardCopyOption;
 public class scDownloader {
 
     private static Path targetPath;
+    private static boolean forceMp3 = true;
 
     public static void Download(String sourceLink, String downloadPath) {
-        soundcloudTrack track = new soundcloudTrack(sourceLink);
+        soundcloudTrack track = new soundcloudTrack(sourceLink, forceMp3);
         try {
             URL sourceURL = new URL(track.getDirectLink());
             targetPath = new File(downloadPath + track.getFilename()).toPath();
@@ -29,31 +31,36 @@ public class scDownloader {
             System.out.println("cant save file");
             e.printStackTrace();
         }
-        try {
-            Mp3File mp3file = new Mp3File(targetPath);
-            ID3v2 id3v2Tag = new ID3v24Tag();
-            id3v2Tag.setArtist(track.getTags().get("artist"));
-            id3v2Tag.setTitle(track.getTags().get("title"));
-            id3v2Tag.setAlbum(track.getTags().get("album"));
-            id3v2Tag.setYear(track.getTags().get("year"));
-            File img = new File("im.jpg");
-            URL aurl = new URL(track.getTags().get("albumArtUrl"));
-            FileUtils.copyURLToFile(aurl, img);
-            RandomAccessFile file = new RandomAccessFile("im.jpg", "r");
-            byte[] bytes = new byte[(int) file.length()];
-            file.read(bytes);
-            file.close();
-            id3v2Tag.setAlbumImage(bytes, "image/jpeg");
-            mp3file.setId3v2Tag(id3v2Tag);
-            mp3file.save(downloadPath + track.getTags().get("artist") + " - " + track.getTags().get("title") + track.getFiletype());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (UnsupportedTagException e) {
-            e.printStackTrace();
-        } catch (InvalidDataException e) {
-            e.printStackTrace();
-        } catch (NotSupportedException e) {
-            e.printStackTrace();
+        if (forceMp3 == true) {
+            try {
+                Mp3File mp3file = new Mp3File(targetPath);
+                ID3v2 id3v2Tag = new ID3v24Tag();
+                id3v2Tag.setArtist(track.getTags().get("artist"));
+                id3v2Tag.setTitle(track.getTags().get("title"));
+                id3v2Tag.setAlbum(track.getTags().get("album"));
+                id3v2Tag.setYear(track.getTags().get("year"));
+                URL aurl = new URL(track.getTags().get("albumArtUrl"));
+                InputStream in = aurl.openStream();
+                byte[] bytes = in.readAllBytes();
+                id3v2Tag.setAlbumImage(bytes, "image/jpeg");
+                mp3file.setId3v2Tag(id3v2Tag);
+                mp3file.save(downloadPath + track.getTags().get("artist") + " - " + track.getTags().get("title") + track.getFiletype());
+                try
+                {
+                    File f = new File(downloadPath + track.getFilename());
+                    f.delete();
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (UnsupportedTagException e) {
+                e.printStackTrace();
+            } catch (InvalidDataException e) {
+                e.printStackTrace();
+            } catch (NotSupportedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/com/wairacynical/soundcloudTrack.java
+++ src/main/java/com/wairacynical/soundcloudTrack.java
@@ -12,7 +12,10 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class soundcloudTrack {
     private static final OkHttpClient httpClient = new OkHttpClient();
@@ -20,6 +23,7 @@ public class soundcloudTrack {
     private String directLink;
     private String filetype;
     private String filename;
+    private boolean forceMp3;
     private Map<String,String> tags = new LinkedHashMap<>();
 
     private final String reserveClientId = "c58TXg96mhC1ETLDBCdIhbGdzSHdzqXN";
@@ -29,8 +33,9 @@ public class soundcloudTrack {
     private final String part3="https://cf-hls-media.sndcdn.com/media/";
     private final String MAX_LENGHT = "9000000";
 
-    public soundcloudTrack(String sourceLink) {
+    public soundcloudTrack(String sourceLink, boolean forceMp3) {
         this.sourceLink = sourceLink;
+        this.forceMp3 = forceMp3;
         makeTags();
         makeDirectLink();
         makeFilename();
@@ -106,26 +111,49 @@ public class soundcloudTrack {
     }
 
     private void makeDirectLink() {
+        Pattern pattern = Pattern.compile("https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)");
+        LinkedList<String> matchedLinks = new LinkedList<>();
+        StringBuilder str = new StringBuilder();
         try {
-            Document doc = Jsoup.connect(sourceLink).get();
-            String mediaLink = doc.getAllElements().toString();
-            //System.out.println(mediaLink);
-            mediaLink = mediaLink.substring(mediaLink.indexOf(part1), mediaLink.indexOf(part2));
-            StringBuilder str = new StringBuilder()
-                    .append(mediaLink)
-                    .append("/stream/hls?client_id=")
-                    .append(reserveClientId);
-            //System.out.println("requestLink = " + str.toString());
-            mediaLink = getRequestToJsonGetUrl(str.toString());
-            //System.out.println("mediaLink = " + mediaLink);
-            URL website = new URL(mediaLink);
-            InputStream in = website.openStream();
-            byte[] bytes = in.readAllBytes();
-            String playlist = new String(bytes, StandardCharsets.UTF_8);
-            //System.out.println("playlist = " + playlist);
-            directLink = playlist.substring(playlist.lastIndexOf(part3), playlist.lastIndexOf("#"));
-            //System.out.println(directLink);
-            directLink = directLink.replaceAll("\\/media\\/.*\\/.*\\/", "/media/0/" + MAX_LENGHT +"/" );
+            //get all stream links from page
+            Matcher matcher = pattern.matcher(Jsoup.connect(sourceLink)
+                                                            .get()
+                                                            .getAllElements()
+                                                            .toString());
+            while (matcher.find()) {
+                if(matcher.group().contains("stream/hls"))
+                matchedLinks.add(matcher.group());
+            }
+            //matchedLinks.forEach(System.out::println);
+            //choose correct link depending on filetype
+            if (forceMp3 == true) {
+                str.append(matchedLinks.get(0))
+                        .append("?client_id=")
+                        .append(reserveClientId);
+            }
+            // TODO: forceMp3 false behaviour
+//            else {
+//                str.append(matchedLinks.get(1))
+//                        .append("?client_id=")
+//                        .append(reserveClientId);
+//                }
+                //get playlist link
+                String buffer = getRequestToJsonGetUrl(str.toString());
+                //System.out.println("playlistLink = " + buffer);
+                //get data from playlist
+                URL playlistLink = new URL(buffer);
+                InputStream in = playlistLink.openStream();
+                byte[] bytes = in.readAllBytes();
+                buffer = new String(bytes, StandardCharsets.UTF_8);
+                //System.out.println("buffer = " + buffer);
+                // get direct link from playlist
+                matcher = pattern.matcher(buffer);
+                matcher.find();
+                buffer = matcher.group();
+                //edit direct link
+                //System.out.println("directLink = " + buffer);
+                directLink = buffer.replaceAll("\\/media\\/.*\\/.*\\/", "/media/0/" + MAX_LENGHT + "/");
+                //System.out.println("directLink = " + directLink);
         } catch (IOException e) {
             System.out.println("cant get mediaLink from sourceLink");
             e.printStackTrace();
@@ -154,7 +182,7 @@ public class soundcloudTrack {
             filename = "track";
         }
     }
-    public String getRequestToJsonGetUrl(String link) throws IOException {
+    private String getRequestToJsonGetUrl(String link) throws IOException {
         Request r = new Request.Builder()
                 .url(link)
                // .addHeader("User-Agent", agent)
